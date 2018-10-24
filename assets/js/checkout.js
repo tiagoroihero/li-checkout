63010df41d385658579cee5a2be40809f37ab335
Tiago Ferreira <tiago@mobfacil.com>
2018-10-24 14:51:19 -0300

    Commit inicial

---
+++ assets/js/checkout.js
@@ -0,0 +1,109 @@
+var doSubmit = false;
+
+window.onload = function() {
+	
+	addEvent(document.querySelector('#pay'),'submit',doPay);
+	addEvent(document.querySelector('input[data-checkout="cardNumber"]'), 'keyup', guessingPaymentMethod);
+	addEvent(document.querySelector('input[data-checkout="cardNumber"]'), 'change', guessingPaymentMethod);
+	
+	// APAGAR essa chamada e a função
+	setTimeout(() => {
+		popularForm();
+		
+		Mercadopago.setPublishableKey("TEST-8c632497-e8e3-4101-b273-9455f15379b7");
+		Mercadopago.getIdentificationTypes();
+		
+	}, 100);
+}
+
+
+function popularForm() {
+	document.getElementById('cardNumber').value = '5598 3704 5728 7955';
+	document.getElementById('securityCode').value = '385';
+	document.getElementById('cardExpirationMonth').value = '01';
+	document.getElementById('cardExpirationYear').value = '2020';
+	document.getElementById('cardholderName').value = 'Olaf do filme frozen';
+	document.getElementById('docNumber').value = '88062398056';
+}
+
+
+function doPay(event) {
+    event.preventDefault();
+    if(!doSubmit){
+        var $form = document.querySelector('#pay');
+        
+        Mercadopago.createToken($form, sdkResponseHandler); // The function "sdkResponseHandler" is defined below
+
+        return false;
+    }
+};
+
+function addEvent(el, eventName, handler){
+    if (el.addEventListener) {
+           el.addEventListener(eventName, handler);
+    } else {
+        el.attachEvent('on' + eventName, function(){
+          handler.call(el);
+        });
+    }
+};
+
+function getBin() {
+    var ccNumber = document.querySelector('input[data-checkout="cardNumber"]');
+    return ccNumber.value.replace(/[ .-]/g, '').slice(0, 6);
+};
+
+function guessingPaymentMethod(event) {
+    var bin = getBin();
+
+    if (event.type == "keyup") {
+        if (bin.length >= 6) {
+            Mercadopago.getPaymentMethod({
+                "bin": bin
+            }, setPaymentMethodInfo);
+        }
+    } else {
+        setTimeout(function() {
+            if (bin.length >= 6) {
+                Mercadopago.getPaymentMethod({
+                    "bin": bin
+                }, setPaymentMethodInfo);
+            }
+        }, 100);
+    }
+};
+
+function setPaymentMethodInfo(status, response) {
+    if (status == 200) {
+        // do somethings ex: show logo of the payment method
+        var form = document.querySelector('#pay');
+
+        if (document.querySelector("input[name=paymentMethodId]") == null) {
+            var paymentMethod = document.createElement('input');
+            paymentMethod.setAttribute('name', "paymentMethodId");
+            paymentMethod.setAttribute('type', "hidden");
+            paymentMethod.setAttribute('value', response[0].id);
+
+            form.appendChild(paymentMethod);
+        } else {
+            document.querySelector("input[name=paymentMethodId]").value = response[0].id;
+        }
+    }
+};
+
+function sdkResponseHandler(status, response) {
+    if (status != 200 && status != 201) {
+        alert("verify filled data");
+    }else{
+       
+        var form = document.querySelector('#pay');
+
+        var card = document.createElement('input');
+        card.setAttribute('name',"token");
+        card.setAttribute('type',"hidden");
+        card.setAttribute('value',response.id);
+        form.appendChild(card);
+        doSubmit=true;
+        form.submit();
+    }
+};
---
+++ payment/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once ('mercadopago.php');
+
+$mp = new MP('TEST-5595184022048439-102214-1cfc3b276b896ba45756121ca2644cf1-363292093');
+
+print_r($_POST);
+
+$payment_data = array(
+        "transaction_amount" => 100,
+        "token" => "ff8080814c11e237014c1ff593b57b4d",
+        "description" => "Title of what you are paying for",
+        "installments" => 1,
+        "payment_method_id" => "visa",
+        "payer" => array (
+                "email" => "tiago.ferreira@roihero.com.br"
+        )
+);
+
+$payment = $mp->post("/v1/payments", $payment_data);
+?>
