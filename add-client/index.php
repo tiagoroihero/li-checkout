9731ad6a546fc07bdf172abe8a8da61d58e3c53b
Tiago Ferreira <tiago@mobfacil.com>
2018-10-29 11:04:06 -0300

    Ajuste

--- add-client/index.php
+++ add-client/index.php
@@ -7,7 +7,23 @@ $mp = new MP('TEST-5595184022048439-102214-1cfc3b276b896ba45756121ca2644cf1-3632
 // Produção
 //$mp = new MP('APP_USR-5595184022048439-102214-ae8266d7c6a6309dd752271cb74247d3-363292093');
 
-$customer = $mp->post ("/v1/customers", array("email" => "test1@test.com"));
+
+try {
+    
+    $customer = $mp->post ("/v1/customers", array("email" => "test1@test.com"));
+    
+} catch (MercadoPagoException $e) {
+    
+    echo 'Message: ', $e->getMessage(), '<br/>Error: ', $e->getCode(), '<br/>Trace: ', $e->getTraceAsString(), '<br/>';
+    
+    var_dump($e);
+    
+} finally {
+    
+    echo 'passou aqui no finally<br/>';
+}
+
+
 
 // echo 'Status: ', $customer['status'], '<hr/>';
 //echo 'Id: ', $customer['response']['id'], '<hr/>';
